d0f7d62b2cc9c1e1f12eec0a9f427e10c19cab26
Keval Panchal <kevalpanchal@siliconithub.com>
2022-03-25 17:21:03 +0530

    update.

--- src/page/Login.js
+++ src/page/Login.js
@@ -88,7 +88,7 @@ const Login = observer((props) => {
 	};
 
 	useEffect(() => {
-		console.log("object : ","version latest");
+		console.log("object : ","version latest"); 
 		if (remember_me) {
 			form.setFieldsValue({
 				email: remember_me.email,
